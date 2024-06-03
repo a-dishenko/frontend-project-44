61b1d5c369f78e24606865bf2b7a6af2f091aea9
teknomad-gudilap <teknomad@gudilap.ru>
2024-06-03 23:03:43 +0300

    brain-prime implemented

---
+++ src/games/prime.js
@@ -0,0 +1,37 @@
+import { doAsking, greeting } from '../index.js';
+
+const primeGame = () => {
+  const props = {
+    startQuestion: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    name: greeting(),
+  };
+
+  const getQuestion = () => {
+    const isPrime = (num2test) => {
+      let result = true;
+      for (let i = Math.floor(num2test / 2); i > 1; i -= 1) {
+        if (num2test % i === 0) {
+          result = false;
+          break;
+        }
+      }
+      return result;
+    };
+
+    const num2guess = Math.floor(Math.random() * 100) + 1;
+    const question = `Question: ${num2guess}`;
+    return { text: question, value: num2guess, isPrime: isPrime(num2guess) };
+  };
+
+  const isCorrect = (question, answerRaw) => {
+    const answer = answerRaw.toLowerCase();
+    if (['yes', 'no'].indexOf(answer) === -1) return false;
+    if (answer === 'yes' && question.isPrime === true) return true;
+    if (answer === 'no' && question.isPrime === false) return true;
+    return false;
+  };
+
+  doAsking(isCorrect, getQuestion, props);
+};
+
+export default primeGame;
